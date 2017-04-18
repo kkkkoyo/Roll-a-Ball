f7ebea11d81ab2b9fde8e37efe43180d76f44a9b
kkkkoyo_ <mmkrz.sg@gmail.com>
2017-04-18 16:13:34 +0900

    add title

---
+++ Assets/_Scripts/game/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    public Text scoreLabel;
+	[SerializeField] private GameObject winnerLabelObject;
+    public void Update ()
+    {
+        int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+        scoreLabel.text = count.ToString ();
+		if (count == 0) {
+            // オブジェクトをアクティブにする
+            winnerLabelObject.SetActive (true);
+        }
+    }
+}
